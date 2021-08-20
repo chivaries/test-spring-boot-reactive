b74945b1724667efd11d3b5c1766cd87da60fca8
glamrock <chivaries@gmail.com>
2021-08-20 14:08:14 +0900

    Initial commit

---
+++ src/main/java/com/example/glamrock/test/reactive/TestSpringBootReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.glamrock.test.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringBootReactiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/glamrock/test/reactive/TestSpringBootReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glamrock.test.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
